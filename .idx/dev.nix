97535b846bc85e354904288b7f84182529a646a1
patelvk2000 <patelvk2000@gmail.com>
2024-05-28 22:47:59 +0000

    Latest canary

---
+++ .idx/dev.nix
@@ -0,0 +1,13 @@
+{ pkgs }: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.bun
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    "bradlc.vscode-tailwindcss"
+    "esbenp.prettier-vscode"
+    "streetsidesoftware.code-spell-checker"
+    "WakaTime.vscode-wakatime"
+  ];
+}
